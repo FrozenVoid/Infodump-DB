8050b098861a09e9fe6db4db071602de628907d7
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-02-07 21:31:27 +0200

    Update pairtropy.c

--- Programming/C/Randomness/pairtropy.c
+++ Programming/C/Randomness/pairtropy.c
@@ -1,7 +1,8 @@
-f32 pairtropy(u8* data,size_t len){
-//return maxpairs/0pairs+1
- u64 dev[65536]={0};u64 lenm=1;
+f64 pairtropy(u8* data,size_t len){
+//return  pair density 1.0+
+ u64 dev[65536]={0};f64 lenm=1.0;
 for(size_t i=0;i<len-1;i++)dev[(data[i]<<8)+data[i+1]]++;
-for(size_t i=0;i<65536;i++){lenm+=(dev[i]==0);}
-return (65536.0/lenm);
+for(size_t i=0;i<65536;i++){
+f64 r=1.0/(dev[i]+1);lenm+=r*r*r;}
+return ((65536.0/lenm));
 }
